6e62dee0dc15c098435ec498c339992c5928f879
103tomi <t.tomi244@gmail.com>
2023-07-28 23:16:37 +0900

    2回目コミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  $(function () {
+    // ハンバーガーメニュー
+    $(".js-hamburger,.js-drawer").click(function () {
+      $(".js-hamburger").toggleClass("is-active");
+      $(".js-drawer").fadeToggle();
+    });
+  });
 
 });
