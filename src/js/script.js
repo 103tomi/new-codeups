3d535e3560d98a284ac3216983073012a13c2f05
103tomi <t.tomi244@gmail.com>
2023-07-30 00:35:48 +0900

    3回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,27 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
   });
 
+  // ドロワーメニュー自動で閉じる
+$(window).on("resize", function() {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    closeDrawer();
+    resetHamburger();
+  }
+});
+
+function openDrawer() {
+  $(".js-drawer").fadeIn();
+  $(".js-hamburger").addClass("is-open");
+}
+
+function closeDrawer() {
+  $(".js-drawer").fadeOut();
+}
+
+function resetHamburger() {
+  if ($(".js-hamburger").hasClass("is-active")) {
+    $(".js-hamburger").removeClass("is-active");
+  }
+}
+
 });
