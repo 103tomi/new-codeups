cddc6a174743870f65ced056a3a3fa83f5625035
103tomi <t.tomi244@gmail.com>
2023-08-06 19:15:46 +0900

    11回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,32 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  $(function () {
+
     // ハンバーガーメニュー
     $(".js-hamburger,.js-drawer").click(function () {
       $(".js-hamburger").toggleClass("is-active");
       $(".js-drawer").fadeToggle();
     });
-  });
 
-  // ドロワーメニュー自動で閉じる
-  $(window).on("resize", function () {
-    if (window.matchMedia("(min-width: 768px)").matches) {
-      closeDrawer();
-      resetHamburger();
-    }
-  });
 
-  function openDrawer() {
-    $(".js-drawer").fadeIn();
-    $(".js-hamburger").addClass("is-open");
-  }
-
-  function closeDrawer() {
-    $(".js-drawer").fadeOut();
-  }
-
-  function resetHamburger() {
-    if ($(".js-hamburger").hasClass("is-active")) {
-      $(".js-hamburger").removeClass("is-active");
-    }
-  }
-
-  // // メインビューのスライダー
-  var swiper = new Swiper(".js-mv-swiper", {
-    loop: true,
-    effect: "fade",
-    speed: 3000,
-    autoplay: {
-      delay: 3000,
-    },
-  });
-
+    
   // キャンペーンのスライダー
   var swiper = new Swiper(".js-campaign-swiper", {
     loop: true,
     speed: 1500,
-    slidesPerView: 1.2,
+    slidesPerView: 1.3,
     spaceBetween: 24,
     allowTouchMove: false,
 
     breakpoints: {
       768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
+        slidesPerView: 3.45,
+        spaceBetween: 35,
       },
     },
 
     autoplay: {
-      delay: 1500,
+      delay: 2000,
       disableOnInteraction: false,
     },
 
@@ -68,12 +36,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     },
   });
 
-
-  //要素の取得とスピードの設定
+// カラーアニメーション
   var box = $('.js-effect'),
     speed = 700;
 
-  //.colorboxの付いた全ての要素に対して下記の処理を行う
   box.each(function () {
     $(this).append('<div class="color"></div>')
     var color = $(this).find($('.color')),
@@ -82,7 +48,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
     image.css('opacity', '0');
     color.css('width', '0%');
-    //inviewを使って背景色が画面に現れたら処理をする
+
     color.on('inview', function () {
       if (counter == 0) {
         $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
@@ -95,4 +61,25 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
   });
 
+  //TOPへ戻るボタン
+  $(function () {
+    const pageTop = $(".js-page-top");
+    pageTop.hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 800) {
+        pageTop.fadeIn();
+      } else {
+        pageTop.fadeOut();
+      }
+    });
+    pageTop.click(function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        300
+      );
+      return false;
+    });
+  });
 });
